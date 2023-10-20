1eb73bca75607abfdf30085238cb6ab4d675dfd8
Tirey Morris <3528600+tireymorris@users.noreply.github.com>
2023-10-19 21:06:36 -0500

    skeleton address form page

---
+++ app/controllers/weather_controller.rb
@@ -0,0 +1,8 @@
+# app/controllers/weather_controller.rb
+class WeatherController < ApplicationController
+  def index
+    # Stub forecast data (you can replace this with real data later)
+    @forecast = "Today's Forecast: Sunny with a high of 75°F"
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'weather/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +8,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # config/routes.rb
+  Rails.application.routes.draw do
+    get 'weather/index'
+    root 'weather#index' # Set the root path to the Weather#index action
+  end
+
 end
