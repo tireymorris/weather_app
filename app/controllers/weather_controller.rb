02fcb579745adfe43a26e70bdbdb851e94e153dc
Tirey Morris <3528600+tireymorris@users.noreply.github.com>
2023-10-21 10:50:00 -0500

    call geocoding API first, and refactor method to use early returns

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,29 +1,29 @@
-# app/controllers/weather_controller.rb
 require 'net/http'
 require 'json'
 
 class WeatherController < ApplicationController
   def index
     zip_code = params[:zip_code]
-    if zip_code.present?
-      api_key = ENV['WEATHER_API_KEY']
-      url = "https://api.openweathermap.org/data/3.0/onecall?zip=#{zip_code}&appid=#{api_key}"
-      
-      response = Net::HTTP.get(URI(url))
-      data = JSON.parse(response)
-      
-      puts 'API KEY: ', api_key
-      puts 'DATA:', data
-      
-      if data['main']
-        temperature = data['main']['temp']
-        @forecast = "Current Temperature: #{temperature}°F"
-      else
-        @forecast = "Forecast not available for this ZIP code."
-      end
-    else
-      @forecast = "Enter a ZIP code to get the forecast."
-    end
+    return @forecast = "Enter a ZIP code to get the forecast." unless zip_code.present?
+
+    api_key = ENV['WEATHER_API_KEY']
+
+    geocoding_url = "http://api.openweathermap.org/geo/1.0/zip?zip=#{zip_code},US&appid=#{api_key}"
+    geocoding_response = Net::HTTP.get(URI(geocoding_url))
+    geocoding_data = JSON.parse(geocoding_response)
+    
+    return @forecast = "Location not found for the provided ZIP code." unless geocoding_data['lat'] && geocoding_data['lon']
+
+    lat = geocoding_data['lat']
+    lon = geocoding_data['lon']
+    forecast_url = "https://api.openweathermap.org/data/3.0/onecall?lat=#{lat}&lon=#{lon}&exclude=hourly,daily&appid=#{api_key}"
+    forecast_response = Net::HTTP.get(URI(forecast_url))
+    forecast_data = JSON.parse(forecast_response)
+
+    return @forecast = "Forecast not available for this location." unless forecast_data['current']
+
+    temperature = forecast_data['current']['temp']
+    @forecast = "Current Temperature: #{temperature}°F"
   end
 end
 
