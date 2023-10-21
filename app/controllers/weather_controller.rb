10780b0b40b8cbe97fccea312e1a6ed7e22e7f3e
Tirey Morris <3528600+tireymorris@users.noreply.github.com>
2023-10-21 10:43:37 -0500

    Add API Call (currently key is inactive, will need to wait a couple hours before using

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,8 +1,29 @@
 # app/controllers/weather_controller.rb
+require 'net/http'
+require 'json'
+
 class WeatherController < ApplicationController
   def index
-    # Stub forecast data (you can replace this with real data later)
-    @forecast = "Today's Forecast: Sunny with a high of 75°F"
+    zip_code = params[:zip_code]
+    if zip_code.present?
+      api_key = ENV['WEATHER_API_KEY']
+      url = "https://api.openweathermap.org/data/3.0/onecall?zip=#{zip_code}&appid=#{api_key}"
+      
+      response = Net::HTTP.get(URI(url))
+      data = JSON.parse(response)
+      
+      puts 'API KEY: ', api_key
+      puts 'DATA:', data
+      
+      if data['main']
+        temperature = data['main']['temp']
+        @forecast = "Current Temperature: #{temperature}°F"
+      else
+        @forecast = "Forecast not available for this ZIP code."
+      end
+    else
+      @forecast = "Enter a ZIP code to get the forecast."
+    end
   end
 end
 
--- test/controllers/weather_controller_test.rb
+++ test/controllers/weather_controller_test.rb
@@ -1,8 +1,10 @@
 require "test_helper"
 
 class WeatherControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get weather_index_url
+  test "should get forecast for a valid ZIP code" do
+    get weather_index_path(zip_code: "90210")
     assert_response :success
+    assert_select "p", "Forecast:"
   end
 end
+
