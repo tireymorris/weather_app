c1eee85742a746437c450d9dbf4c709991177b04
Tirey Morris <3528600+tireymorris@users.noreply.github.com>
2023-10-21 11:42:39 -0500

    add much more information from API

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -23,19 +23,13 @@ class WeatherController < ApplicationController
       # Step 2: Forecast API call using latitude and longitude
       lat = geocoding_data['lat']
       lon = geocoding_data['lon']
-      forecast_url = "https://api.openweathermap.org/data/3.0/onecall?lat=#{lat}&lon=#{lon}&exclude=hourly,daily&appid=#{api_key}&units=imperial"
+      forecast_url = "https://api.openweathermap.org/data/3.0/onecall?lat=#{lat}&lon=#{lon}&exclude=minutely,hourly,alerts&appid=#{api_key}&units=imperial"
       forecast_response = Net::HTTP.get(URI(forecast_url))
       forecast_data = JSON.parse(forecast_response)
   
       return @forecast = "Forecast not available for this location." unless forecast_data['current']
 
-      # Extract relevant data from the API response
-      current_weather = forecast_data['current']
-      temperature = current_weather['temp']
-      weather_description = current_weather['weather'][0]['description']
-
-      # Construct a user-friendly forecast message
-      @forecast = "Current Temperature: #{temperature}°F. Weather: #{weather_description}"
+      @forecast = forecast_data
 
       # Cache the forecast and alerts for 30 minutes
       Rails.cache.write(zip_code, @forecast, expires_in: 30.minutes)
